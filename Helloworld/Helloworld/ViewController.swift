ddff9e34a9a939555d2485c8a408ec70c75dc721
Gebruiker <delap007@planet.nl>
2017-01-18 12:29:48 +0100

    New branch

--- Helloworld/Helloworld/ViewController.swift
+++ Helloworld/Helloworld/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var calculation: UILabel!
+    @IBOutlet weak var outputlabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func PrintHelloWorld(_ sender: Any) {
+        let helloWorld = "HelloWorld"
+        outputlabel.text = helloWorld
+    }
+    
+    @IBAction func calculation(_ sender: Any) {
+        let multiply =  3 * 12
+        calculation.text = String(multiply)
+    }
+    
+    
+    
 
 }
+
 
